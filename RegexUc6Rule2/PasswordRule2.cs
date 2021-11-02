82992dcb3e2b28563c42c57ba7f6652ccb8038af
vishal <vishalrathod7038@gmail.com>
2021-11-02 11:04:42 +0530

    [vishal] RegExUc6 Rule2 Adding  number in passwaor validation

---
+++ RegexUc6Rule2/PasswordRule2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUc6Rule2
+{
+    class PasswordRule2
+    {
+
+
+        string[] Inputs = { "Vishalhi", "abcvsdcv", "xyzYzvab", "Vishalrathod", "abcds", "Abcdsgx", "rajubhaI" };
+        string passPattern = "^(?=.*[A-Z])[a-zA-Z]{8}$";
+      
+        public void Validation()
+        {
+            Regex regex = new Regex(passPattern);
+            Console.WriteLine(" password Validation ");
+            foreach (string word in Inputs)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + "---> valid");
+                }
+                else
+                {
+                    Console.WriteLine(word + "---> invalid");
+                }
+            }
+
+        }
+    }
+}
---
+++ RegexUc6Rule2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegexUc6Rule2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PasswordRule2 password = new PasswordRule2();
+            password.Validation();
+            Console.ReadLine();
+        }
+    }
+}

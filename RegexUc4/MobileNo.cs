5debeb70be6411fcd8e3ad1707260b38e2862eb0
vishal <vishalrathod7038@gmail.com>
2021-11-02 10:56:30 +0530

    [Vishal] RegExUc4 Mobile Number Validation

---
+++ RegexUc4/MobileNo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUc4
+{
+    class MobileNo
+    {
+        string[] Inputs = { "91 9986522051", "01 6886531051", "918686531051", "91 86865310", "91 6223344556", "91 868653105112" };
+        string mobileNoPattern = "[1-9]{2}[ ]{1}[0-9]{10}";
+
+        public void Validation()
+        {
+            Regex regex = new Regex(mobileNoPattern);
+            Console.WriteLine(" Mobile Validation ");
+            foreach (string word in Inputs)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + "---> valid");
+                }
+                else
+                {
+                    Console.WriteLine(word + "---> invalid");
+                }
+            }
+
+        }
+    }
+}
---
+++ RegexUc4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegexUc4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MobileNo mobile = new MobileNo();
+            mobile.Validation();
+            Console.ReadLine();
+        }
+    }
+}

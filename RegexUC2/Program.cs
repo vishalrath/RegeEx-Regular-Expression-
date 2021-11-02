3fdb734e0c3786183a3d3ad803282eef9104979d
vishal <vishalrathod7038@gmail.com>
2021-11-02 10:46:40 +0530

    [Vishal] RegExUc2 enter valide LastName

---
+++ RegexUC2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RegexUC2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            RegexLastName registration = new RegexLastName();
+                registration.LastName();
+
+                Console.WriteLine("Welcome to User Registration");
+                Console.WriteLine("\n Please enter  First Name--");
+                string LastName = Console.ReadLine();
+            RegexLastName.LastName(LastName);                                          //taking user input and passing it in first name method of regular exp class
+                Console.ReadLine();
+            }
+    }
+}
---
+++ RegexUC2/RegexLastName.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUC2
+{
+    class RegexLastName
+    {
+            // taking inputs fron user
+        public static void LastName(string name)                   //declaring the static method 
+        {
+            string LastName = "^[A-Z]{1}[a-zA-Z]{2,}$";            //this regular exp shows that first letter must be capital, and minimum letter must be 3
+            Regex regex = new Regex(LastName);
+            if (regex.IsMatch(name))                               //calling the Regex class's functions
+            {
+                Console.WriteLine(name + " is valid");
+            }
+            else
+                Console.WriteLine(name + " is invalid. Please check your name and start with capital letter.");
+        }
+
+          //defualt input
+        string[] inputs = { "Rathod", "Jadhav", "rat", "chavhan", "abc" };
+        string pattern = "^[A-Z]{1}[a-zA-Z]{2,}$";            //this regular exp shows that first letter must be capital, and minimum letter must be 3
+
+        
+         public void LastName()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("last name   Validation ");
+            foreach (string word in inputs)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + "---> valid");
+                }
+                else
+                {
+                    Console.WriteLine(word + "---> invalid");
+                }
+            }
+
+        }
+    }
+}

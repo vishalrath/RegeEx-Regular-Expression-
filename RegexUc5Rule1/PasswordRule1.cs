a20660fded37dbf1960d44817978e5354f64f257
vishal <vishalrathod7038@gmail.com>
2021-11-02 11:00:07 +0530

    [Vishal] RegExUc5 Rule1 Validation of Password

---
+++ RegexUc5Rule1/PasswordRule1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUc5Rule1
+{
+    class PasswordRule1
+    {
+        
+        string[] passwordInputs = { "vishalfh", "bdhdb", "Vishal", "Vishalrathod", "rathodab" };
+        string passPattern = "^[a-z]{8}$";
+
+        public void Validation()
+        {
+            Regex regex = new Regex(passPattern);
+            Console.WriteLine(" password Validation ");
+            foreach (string word in passwordInputs)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + "---> valid");
+                }
+                else
+                {
+                    Console.WriteLine(word + "---> invalid");
+                }
+            }
+
+        }
+    }
+}
+
---
+++ RegexUc5Rule1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegexUc5Rule1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PasswordRule1 password = new PasswordRule1();
+            password.Validation();
+            Console.ReadLine();
+        }
+    }
+}

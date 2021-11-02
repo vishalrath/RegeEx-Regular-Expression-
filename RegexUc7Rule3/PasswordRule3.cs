95c0772c61a949e9ddef9846a323f807563140b4
vishal <vishalrathod7038@gmail.com>
2021-11-02 11:09:01 +0530

    [Vishal] RegExUc7 Rule3 Adding atleast one Number And One Capital Letter

---
+++ RegexUc7Rule3/PasswordRule3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUc7Rule3
+{
+    class PasswordRule3
+    {
+        string[] Inputs = { "Vishalh1", "abc1sdcv", "xyzYzv3b", "Vishalrathod", "abcds", "Abcds0x", "raju8haI" };
+        string passPattern = "^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{8}$";
+
+        public void Validation()
+        {
+            Regex regex = new Regex(passPattern);
+            Console.WriteLine(" password Validation ");
+            foreach (string word in Inputs)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + "---> valid");
+                }
+                else
+                {
+                    Console.WriteLine(word + "---> invalid");
+                }
+            }
+
+        }
+    }
+}
---
+++ RegexUc7Rule3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RegexUc7Rule3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PasswordRule3 pass = new PasswordRule3();
+            pass.Validation();
+        }
+    }
+}

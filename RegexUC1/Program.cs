87768ab05c05b75da3b39df52c1258445ba0c9f5
vishal <vishalrathod7038@gmail.com>
2021-11-02 10:39:19 +0530

    [Vishal] Uc1 Using Regex enter valid first name

---
+++ RegexUC1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RegexUC1
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+           Console.WriteLine("Hello World!");
+           RegxPattern registration = new RegxPattern();
+            registration.FirstName();
+
+            Console.WriteLine("Welcome to User Registration");
+            Console.WriteLine("\n Please enter  First Name--");
+            string firstName = Console.ReadLine();
+            RegxPattern.FirstName(firstName);                                          //taking user input and passing it in first name method of regular exp class
+            Console.ReadLine();
+        }
+    }
+}

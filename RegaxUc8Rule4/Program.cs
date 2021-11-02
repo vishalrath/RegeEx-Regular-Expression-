227987026712a718279cc91502994ceb5b26166d
vishal <vishalrathod7038@gmail.com>
2021-11-02 11:15:37 +0530

    [Visha] RegExUc8 Rule4 Combination of Password validation.

---
+++ RegaxUc8Rule4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegaxUc8Rule4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            passordRule4 pass = new passordRule4();
+            pass.Validation();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegaxUc8Rule4/passordRule4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegaxUc8Rule4
+{
+    class passordRule4
+    {
+
+        string[] Inputs = { "Vish@lh1", "abc1sd&v", "$yzYzv3b", "Vishal1ath@d", "abcds", "#Abcds0x", "raj^8haI","hvfcg5*" };
+        string passPattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8}$";
+
+
+        public void Validation()
+        {
+            Regex regex = new Regex(passPattern);
+            Console.WriteLine(" password Validation ");
+            foreach (string word in Inputs)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + "---> valid");
+                }
+                else
+                {
+                    Console.WriteLine(word + "---> invalid");
+                }
+            }
+
+        }
+    }
+}
